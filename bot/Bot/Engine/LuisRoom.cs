0902df0c60be8d71f2e9bad592d9b35094cf9fa8
Edwin van Wijk <edwinvw@live.nl>
2017-09-27 12:42:43 +0200

    Added Luis intents.

--- bot/Bot/Engine/LuisRoom.cs
+++ bot/Bot/Engine/LuisRoom.cs
@@ -4,10 +4,15 @@ using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
 using Microsoft.Bot.Builder.Luis;
 using Microsoft.Bot.Builder.Luis.Models;
+using System.Collections.Generic;
 
 namespace GameATron3000.Bot.Engine
 {
+    #region secret stuff
     [LuisModel("46904df5-f37e-4beb-bac2-ad6790d45ea5", "9edcfab27acb46b7b430659ab44055d1")]
+    #endregion
+
+    //[LuisModel("my app id", "my app key")]
     [Serializable]
     public abstract class LuisRoom : LuisDialog<object>
     {
@@ -22,10 +27,8 @@ namespace GameATron3000.Bot.Engine
         {
             var roomDefinition = GetRoomDefinition();
 
-            await context.PostEventAsync(Event.EnteredRoom, roomDefinition.ToJObject());
+            await context.PostEventAsync(Event.RoomEntered, roomDefinition.ToJObject());
             await context.PostMessageAsync(roomDefinition.IntroductionText);
-
-            //context.Wait(MessageReceived);
 
             await base.StartAsync(context);
         }
@@ -34,20 +37,16 @@ namespace GameATron3000.Bot.Engine
 
         protected abstract void WireRoom(WireManager wireManager);
 
-        protected IAction StartConversation(string topic)
-        {
-            return new TalkToAction(topic, ResumeAfterConversationTree);
-        }
-
-
         [LuisIntent("")]
         [LuisIntent("None")]
-        public async Task NoneAsync(IDialogContext context, IAwaitable<object> result, LuisResult luisResult)
+        public Task None(IDialogContext context, IAwaitable<object> result, LuisResult luisResult)
         {
-            //context.Wait(this.MessageReceived);
+            return Task.CompletedTask;
         }
 
         [LuisIntent("talk to")]
+        [LuisIntent("look at")]
+        [LuisIntent("pick up")]
         public async Task TalkToAsync(IDialogContext context, IAwaitable<object> result, LuisResult luisResult)
         {
             var activity = (Activity)await result;
@@ -55,10 +54,7 @@ namespace GameATron3000.Bot.Engine
             {
                 await HandleMessageAsync(context, luisResult);
             }
-
-            //context.Wait(this.MessageReceived);
         }
-
 
         private async Task HandleMessageAsync(IDialogContext context, LuisResult luisResult)
         {
@@ -67,19 +63,14 @@ namespace GameATron3000.Bot.Engine
 
             // build activity from LUIS result
             string intent = luisResult.TopScoringIntent.Intent;
-            string actor = luisResult.Entities.Count > 0 ? luisResult.Entities[0].Type : "unknown actor";
-            //EntityRecommendation entity;
-            //if (luisResult.TryFindEntity("simple", out entity))
-            //{
-            //    actor = entity.Role;
-            //}
-            string activityFromLuis = $"{intent} {actor}";
+            string subject = luisResult.Entities.Count > 0 ? luisResult.Entities[0].Type : "unknown";
+            string activityFromLuis = $"{intent} {subject}";
 
+            // execute actions
             var actions = wireManager.GetActions(activityFromLuis, gameState);
-
             foreach (var action in actions)
             {
-                var contextHandled = await action.ExecuteAsync(context);
+                var contextHandled = await action.ExecuteAsync(context, ResumeAfterConversationTree);
                 if (contextHandled)
                 {
                     return;
@@ -94,8 +85,6 @@ namespace GameATron3000.Bot.Engine
             // TODO Check this
             await context.PostMessageAsync("End of conversation!");
             await context.PostEventAsync(Event.Idle);
-
-            //context.Wait(this.MessageReceived);
         }
 
         private WireManager GetWireManager()
