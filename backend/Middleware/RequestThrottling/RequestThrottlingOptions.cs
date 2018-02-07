f96c5ef51f0381905cfacad5f6667dc7effadcd3
Sander Molenkamp <a.molenkamp@gmail.com>
2018-02-07 14:18:46 +0100

    Removed obsolete code

--- backend/Middleware/RequestThrottling/RequestThrottlingOptions.cs
+++
@@ -1,8 +0,0 @@
-namespace backend.Middleware.RequestThrottling
-{
-    public class RequestThrottlingOptions
-    {
-        public int MaxRequestsPer5Seconds { get; set; }
-        public int MaxRequestsPerMinute { get; set; }
-    }
-}
--- bot/Bot/Controllers/MessagesController.cs
+++ bot/Bot/Controllers/MessagesController.cs
@@ -1,5 +1,4 @@
-﻿using System.Linq;
-using System.Net;
+﻿using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
@@ -19,70 +18,9 @@ namespace GameATron3000.Bot.Controllers
         /// </summary>
         public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
         {
-            switch (activity.Type)
-            {
-                case ActivityTypes.ConversationUpdate:
-
-                    // Support for emulator.
-                    // When the bot is added to the emulator, start the game.
-                    if (activity.MembersAdded.Any(m => m.Name == "Game-a-tron 3000 (TM)"
-                        || m.Name == "Bot" || m.Name == "gameatron3000"))
-                    {
-                        await Conversation.SendAsync(activity, () => new RootDialog(new Game()));
-                    }
-                    break;
-
-                case ActivityTypes.Event:
-
-                    // Support for the GUI.
-                    // We need to ensure that the dialog state is saved in the correct user context.
-                    // When the GUI client opens a direct line connection, a conversationUpdate message
-                    // is sent, but it doesn't have the correct From id (should be 'player'). 
-                    // Instead the conversation id is used by the direct line JS implementation. Because
-                    // we need to make sure that dialog state is stored in the 'player' state, we wait
-                    // for an explicit 'start' event with the correct From information before starting 
-                    // the dialog.
-                    if (activity.Name == "start")
-                    {
-                        await Conversation.SendAsync(activity, () => new RootDialog(new Game()));
-                    }
-                    break;
-
-                case ActivityTypes.Message:
-                    await Conversation.SendAsync(activity, () => new RootDialog(new Game()));
-                    break;
-            }
+            await Conversation.SendAsync(activity, () => new RootDialog(new Game()));
 
             return new HttpResponseMessage(HttpStatusCode.Accepted);
-        }
-
-        private Activity HandleSystemMessage(Activity message)
-        {
-            if (message.Type == ActivityTypes.DeleteUserData)
-            {
-                // Implement user deletion here
-                // If we handle user deletion, return a real message
-            }
-            else if (message.Type == ActivityTypes.ConversationUpdate)
-            {
-                // Handle conversation state changes, like members being added and removed
-                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
-                // Not available in all channels
-            }
-            else if (message.Type == ActivityTypes.ContactRelationUpdate)
-            {
-                // Handle add/remove from contact lists
-                // Activity.From + Activity.Action represent what happened
-            }
-            else if (message.Type == ActivityTypes.Typing)
-            {
-                // Handle knowing tha the user is typing
-            }
-            else if (message.Type == ActivityTypes.Ping)
-            {
-            }
-
-            return null;
         }
     }
 }
