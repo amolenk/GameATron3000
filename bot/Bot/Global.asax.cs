0cc185464411205771590aca62ebd150a28ebf0c
Edwin van Wijk <edwinvw@live.nl>
2018-05-02 12:59:57 +0200

    Updated storage implementation + updated all NuGet packages to latest version

--- bot/Bot/Global.asax.cs
+++ bot/Bot/Global.asax.cs
@@ -1,4 +1,9 @@
-﻿using System.Web.Http;
+﻿using Autofac;
+using Microsoft.Bot.Builder.Azure;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Internals;
+using Microsoft.Bot.Connector;
+using System.Web.Http;
 
 namespace GameATron3000.Bot
 {
@@ -6,6 +11,24 @@ namespace GameATron3000.Bot
     {
         protected void Application_Start()
         {
+            var store = new InMemoryDataStore();
+
+            Conversation.UpdateContainer(
+                       builder =>
+                       {
+                           builder.Register(c => store)
+                                     .Keyed<IBotDataStore<BotData>>(AzureModule.Key_DataStore)
+                                     .AsSelf()
+                                     .SingleInstance();
+
+                           builder.Register(c => new CachingBotDataStore(store,
+                                      CachingBotDataStoreConsistencyPolicy
+                                      .ETagBasedConsistency))
+                                      .As<IBotDataStore<BotData>>()
+                                      .AsSelf()
+                                      .InstancePerLifetimeScope();
+                       });
+
             GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
